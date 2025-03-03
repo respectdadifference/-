c5eeadeb6e9e6874e32a858f4ba22e6c6ae6d0d2
respectdadifference <ppk.kej@icloud.com>
2025-03-03 16:11:06 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.style.opacity = "1";
     }, 500);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = "0";
+    setTimeout(() => {
+        document.body.style.transition = "opacity 2s";
+        document.body.style.opacity = "1";
+    }, 500);
+
+    // ギャラリー画像のフェードイン
+    let images = document.querySelectorAll(".gallery-container img");
+    images.forEach((img, index) => {
+        img.style.opacity = "0";
+        setTimeout(() => {
+            img.style.transition = "opacity 1s ease-in-out";
+            img.style.opacity = "1";
+        }, 1000 + index * 500);
+    });
+});
+
